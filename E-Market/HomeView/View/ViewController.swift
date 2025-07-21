39ae58acca7d56a4c7c1de39b7ef93c19e25a795
berk <galata.berk@hotmail.com>
2025-07-21 11:37:48 +0300

    Project created.

---
+++ E-Market/HomeView/View/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  E-Market
+//
+//  Created by Mine Korkmaz on 21.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ E-MarketTests/E_MarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  E_MarketTests.swift
+//  E-MarketTests
+//
+//  Created by Mine Korkmaz on 21.07.2025.
+//
+
+import Testing
+@testable import E_Market
+
+struct E_MarketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
